164d2ca25a5aad261471fe61c81bc833ce565e11
oren <virtualoren@gmail.com>
2018-02-25 12:19:56 -0600

    Initial commit

---
+++ article/index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+//Get the base64 encoded tag, and convert it back into the ID
+$id = (int)base64_decode($_GET['id']);
+	
+try {
+	$conn = new PDO("mysql:host=localhost;dbname=website","guest");
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	$stmt = $conn->query("SELECT * FROM pages WHERE id=" . $id);
+	$row = $stmt->fetch();
+	
+	$title = $row['title'];
+	$pub_date = strtotime($row['pub_date']);
+	$description = $row['description'];
+	$body = $row['body'];
+	$sidebar = $row['sidebar'];
+	$subtitle = $row['subtitle'];
+	$thumbnail = $row['thumbnail'];
+	$header_img = $row['header_img'];
+	
+} catch (PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+	exit();
+}
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <?php 
+    echo '<meta property="og:url" content="orenp.com/article?id=' . base64_encode(sprintf("%03d",$id)) . '"/>'
+    ?>
+    <meta property="og:url" content=""/>
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="<?php echo $title ?>" />
+    <meta property="og:title" content="<?php echo $description?>"/>
+    <meta property="og:image" content="<?php echo $thumbnail?>"/>
+    <meta name="author" content="">
+
+    <title>TITLE</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/css/stylesheet.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="https://orenp.com/research">Research</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/nightduck">Github</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Post Content Column -->
+            <div class="col-lg-8">
+
+                <!-- Blog Post -->
+
+                <!-- Title -->
+                <h1><?php echo $title; ?></h1>
+                <h3><?php echo $subtitle; ?></h3>
+
+                <hr>
+
+                <!-- Date/Time -->
+                <p><span class="glyphicon glyphicon-time"></span> Posted on
+                    <?php
+                    $date = date('M d, Y', $pub_date);
+                    $time = date('H:i A', $pub_date);
+                    echo $date . " at " . $time . " UTC";
+                    ?>
+                </p>
+
+                <!--
+                <hr>
+
+                <img class="img-responsive" src="0" alt="">
+                -->
+
+                <hr>
+
+                <!-- Post Content -->
+		<p class="lead"><?php echo $description; ?></p>
+        <div><?php echo $body ?></div>
+               <!--
+                <hr>
+
+                <-- Blog Comments --
+
+                <-- Comments Form --
+                <div class="well">
+                    <h4>Leave a Comment:</h4>
+                    <form role="form">
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+
+                <hr>
+
+                <-- Posted Comments --
+
+                <-- Comment --
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div>
+
+                <-- Comment --
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <-- Nested Comment --
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading">Nested Start Bootstrap
+                                    <small>August 25, 2014 at 9:30 PM</small>
+                                </h4>
+                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            </div>
+                        </div>
+                        <-- End Nested Comment --
+                    </div>
+                </div>-->
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <div class="col-md-4">
+                <?php echo $sidebar; ?>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!--<!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Contact: inquiries@orenp.com</p>
+                    <a href="https://gandi.net"><img src="/rsc/GANDI_SSL_logo_en.png" alt="Secured by Gandi.net"/></a>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/js/bootstrap.js"></script>
+
+</body></html>
---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+try {
+$conn = new PDO("mysql:host=localhost;dbname=website","guest");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+#When calling the homepage, you can pass parameters that'll dictate what it shows or redirects you to
+$id = $_GET['id'];          #Unique ID of a page, this'll redirect you to that page
+$feed = $_GET['feed'];      #General category (eq to template), this'll redirect you
+$start = $_GET['start'];    #Shows what page you're on. 0 is the first, 10 is the 2nd, etc
+$count = 10;                #Display 10 articles on page
+    
+
+if (!is_null($id)) {
+    #If the URL was passed an ID, then lookup the page and direct to the appropriate template
+    $stmt = $conn->query("SELECT template FROM pages WHERE id=" . (int)base64_decode($id));
+    $row = $stmt->fetch();
+    $path = $row['template'] . "?id=" . $id;
+    header('Location: ' . $path, true);
+    exit();
+}
+
+if (!is_null($start)) {
+    #If there's a start param defined, the user is looking at the 2nd or 3rd page. Use the start as offset
+    #in the SQL query
+    $offset = " OFFSET " . $start;
+} else $offset = "";
+
+if (!is_null($feed)) {
+    #If a feed is specified, the user wants to look at a categorized list of pages. Pass this as a
+    #WHERE template = ... in the SQL query
+    $filter = " WHERE template='" . $feed . "'";
+} else $filter = "";
+
+    #Fetch basic metadata regarding 10 pages
+    #TODO: Let user have a larger feed by making the value 10 dependant on variable
+    $stmt = $conn->query("SELECT id, title, subtitle, description, thumbnail, template
+                                    FROM pages " . $filter . " ORDER BY pub_date desc LIMIT " . str($count) . $offset);
+
+    $pages = $stmt->fetchall();
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit();
+}
+?>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Personal website. I do what I want. Blogging for now. More features to come"/>
+    <meta property="og:url" content="http://orenp.com"/>
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="The Nightduck's Space" />
+    <meta property="og:description" content="Personal website. I do what I want. Blogging for now. More features to come"/>
+    <meta property="og:image" content="http://orenp.com/rsc/insertlogohere.png"/>
+
+    <!--TODO: Modify the title, or add a subtitle dependant on the $feed variable-->
+    <title>The Nightduck's Space</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylesheet.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body data-gr-c-s-loaded="true">
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="color:blue;">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="https://orenp.com/research">Research</a>
+                </li>
+                <li>
+                    <a href="https://github.com/nightduck">Github</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<!-- Page Content -->
+<div class="container">
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Welcome to Oren's Personal Website</h1>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <div class="primary col-md-8 container">
+        <!--
+        <div class="row article">
+            <div class="col-md-4">
+                <a href="/article?id=base64_id_here">
+                    <img class="img-responsive" src="rsc/thumnail_here.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <a href="/template_here?id=base64_id_here">
+                    <h3>Example Item
+                        <br/>
+                        <small>Some of them have subtitles</small>
+                    </h3>
+                </a>
+                <p>Uncomment this section to see what articles should be rendered like</p>
+            </div>
+        </div>
+        -->
+
+        <?php
+        foreach ($pages as $row) {
+            $link = sprintf("/%s?id=%s", $row['template'], base64_encode(sprintf("%03d",$row['id'])));
+            $titleinfo = sprintf("<h3>%s<br/><small>Maybe filled with subtitle</small></h3>",$row['title']);
+
+            echo "<div class='row article'><div class='col-md-4'>";
+            echo sprintf("<a href='%s'>", $link);
+            echo sprintf("<img class='img-responsive' src='rsc/%s' alt=''></a></div>", $row['thumbnail']);
+            echo sprintf("<div class='col-md-8'><a href='%s'>%s</a><p>%s</p>",$link, $titleinfo, $row['description']);
+            echo "</div></div><hr>";
+        }
+        ?>
+
+        <!-- Template
+        <div class="row article">
+            <div class="col-md-4">
+                <a href="/articles/link.html">
+                    <img class="img-responsive" src="rsc/paretocurve.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-8">
+                <a href="/articles/link.html">
+                    <h3>Main Title
+                        <br/>
+                        <small>Subtitle (optional)</small>
+                    </h3>
+                </a>
+                <p>First few words of article. Lorem ipswitchsd  sdlfjls  dijf o
+                   sodij  dofi  jf ijef jfifieeo io  jfe foaeopg jeio d ...</p>
+            </div>
+        </div>
+
+        <hr>-->
+
+    </div>
+    <div class="sidebar col-md-4">
+        <!--TODO: populate sidebar-->
+    </div>
+
+    <!-- <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Contact: inquiries@orenp.com</p>
+                <a href="https://gandi.net"><img src="rsc/GANDI_SSL_logo_en.png" alt="Secured by Gandi.net"/></a>
+            </div>
+        </div>
+        <!-- /.row -->
+    </footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.js"></script>
+
+
+
+
+</body>
+</html> 
